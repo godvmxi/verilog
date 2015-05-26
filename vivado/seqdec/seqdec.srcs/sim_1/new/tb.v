980b23ed160eb6273bc52e0a56464df77c7b03eb
Bright <bright.jiang@infotmic.com.cn>
2015-05-26 19:18:58 +0800

    add seqdec

---
+++ vivado/seqdec/seqdec.srcs/sim_1/new/tb.v
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/26/2015 06:43:19 PM
+// Design Name: 
+// Module Name: tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+'timescale 1ns/1ns 
+
+'define halfperiod 20
+module tb();
+reg clk , rst ;
+reg[22:0] data ;
+wire z ,x ;
+assign  x = data[23];
+
+inital 
+	begin 
+		clk = 0;
+		rst = 1 ;
+		#2 rst = 0;
+		#30 rst = 1 ;
+		data = 20 'b1100_1001_0000_1001_0100 ;
+		#( 'halfperiod * 1000 ) $top ;
+	end
+always #('halfperiod ) clk = ~clk ;
+always @ (posedge clk )
+	#2 data = ( data[22:0] ,data[23];
+
+seqdec m (
+	.x( x ) ,
+	.z( z ) ,
+	.clk ( clk ),
+	.rst( rst )
+);
+
+endmodule
+endmodule
---
+++ vivado/seqdec/seqdec.srcs/sources_1/new/seqdec.v
@@ -0,0 +1,91 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/26/2015 06:42:59 PM
+// Design Name: 
+// Module Name: seqdec
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module seqdec(x,z,clk,rst );
+input x,clk,rst;
+output z;
+reg[2:0] state ;  //state station
+wire z;
+parameter  IDLE = 3'd0,
+            A   = 3'd1,
+            B   = 3'd2,
+            C   = 3'd3,
+            D   = 3'd4,
+            E   = 3'd5,
+            F   = 3'd6,
+            G   = 3'd7;
+ 
+assign  z = ( state == D && x == 0)  ? 1: 0;
+
+always @( posedge clk or negedge rst) 
+    if ( !rst )
+        begin
+            state = IDLE ;
+        end
+    else 
+        casex ( state ) 
+            IDLE :
+				if ( x == 1)
+					state <= A ;
+				else 
+					state <= B ;
+			A   :
+				if ( x == 0 )
+					state <= C ;
+				else 
+					state <= A ;
+			B   :
+				if ( x == 0 )
+					state <= C ;
+				else 
+					state <= F ;
+			C   :
+				if ( x == 0)
+					state <= G ;
+				else 
+					state <= D;
+			D   :
+				if ( x == 0 )
+					state <= E ;
+				else 
+					state <= A ;
+			E   :
+				if ( x == 0 )
+					state <= C ;
+				else 
+					state <= A ;
+			F   :
+				if ( x == 0 )
+					state <= B ;
+				else 
+					state <= A ;
+			G   :
+				if ( x == 0 )
+					state <= F ;
+				else 
+					state <= A ;
+			default :
+					state <= IDLE ;
+		endcase 
+endmodule
+         
+            
