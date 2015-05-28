43c87cd82abd7161eac1dd2bcf323f32cf608808
bright.jiang <bright.jiang@infotmic.com.cn>
2015-05-28 19:49:57 +0800

    add practice 3

--- exercise/3/single_clk.v
+++ exercise/3/single_clk.v
@@ -5,37 +5,40 @@ input start ; // every start 0->1 ,generate a __-----___  wave
 output clk_out;
 
 reg clk_out;
+reg start_flag ;
 `define  delay_uint   5
 `define  clk_last_1   20
-`define  clk_last_2   10
-`define  clk_last_3   20
-reg[7:0]  i ;
-always @(posedge start) begin 
+`define  clk_last_2   30
+`define  clk_last_3   50
+reg[7:0]  counter ;
+always @(posedge clk_in ) begin 
 	if (!rst) begin 
-		clk_out = 0 ;
-		for (i = 0;i< `clk_last_1 ; i = i + 1)
-		begin 
-			@(posedge clk_in) ;
-			# `delay_uint;
-		end
-		clk_out = 1 ;
-		for (i = 0;i< `clk_last_2 ; i = i + 1)
-		begin 
-			@(posedge clk_in) ;
-			# `delay_uint;
-		end
-		clk_out = 0 ;
-		for (i = 0;i< `clk_last_3 ; i = i + 1)
-		begin 
-			@(posedge clk_in) ;
-			# `delay_uint;
-		end
-		clk_out = 1'bz ;
+		counter <= 0;
+		clk_out = 1'bz;
+		start_flag <= 0;
 	end
 	else begin 
-			clk_out = 1'bz ;
-	end 
-
+		if(start_flag) begin 
+			counter <= counter + 1;
+			if (counter < `clk_last_1 )
+				clk_out <= 0;
+			else if ( counter < `clk_last_2 )
+				clk_out <= 1;
+			else if (counter < `clk_last_3 )
+				clk_out <= 0;
+			else begin 
+				clk_out <= 1'bz;
+				start_flag <=  0;
+			end
+		end 
+		else begin 
+			clk_out <= 1'bz;	
+		end
+	end
+end
+always @(posedge start) begin 
+	counter <= 0;
+	start_flag <= 1 ;
 end
 
 endmodule
--- exercise/3/testbench.v
+++ exercise/3/testbench.v
@@ -1,14 +1,14 @@
 // 信号源模型：
-`timescale 1ns/100ps
-`define 	clk_cycle  	50
+`timescale 1ns/1ns
+`define 	clk_cycle  	500
 `include  "./single_clk.v"   //  包含模块文件。在有的仿真调试环境中并不需要此语句。
 
 module  testbench;
 
+wire clk_out;
 reg clk_in ,reset, start;
 
-wire clk_out;
-`define   start_last    50000
+`define   start_last    100000
 always   # `clk_cycle   clk_in  = ~clk_in ;
 
 
@@ -17,8 +17,7 @@ initial              // initial常用于仿真时信号的给出。
 	begin 
 	$dumpfile("wave.vcd");
 	$dumpvars;
-	clk_in = 0;
-	clk_out = 1'z;
+	clk_in  = 1'b0;
 	reset =0 ;
 	start = 0;
 	#`start_last  start = 1;
