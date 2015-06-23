d561cf4f926381ab80cea30cdfbb6976e4fadf72
godvmxi <godvmxi@126.com>
2015-06-23 15:45:06 +0800

    update bus define

--- AZPR_RTL/bus/include/bus.h
+++ AZPR_RTL/bus/include/bus.h
@@ -1,40 +1,40 @@
 /*
  -- ============================================================================
  -- FILE NAME	: bus.h
- -- DESCRIPTION : 僶僗僿僢僟
+ -- DESCRIPTION : 总线数据定义
  -- ----------------------------------------------------------------------------
  -- Revision  Date		  Coding_by	 Comment
- -- 1.0.0	  2011/06/27  suito		 怴婯嶌惉
+ -- 1.0.0	  2011/06/27  suito		 
  -- ============================================================================
 */
 
 `ifndef __BUS_HEADER__
-	`define __BUS_HEADER__			 // 僀儞僋儖乕僪僈乕僪
+	`define __BUS_HEADER__			 // 
 
-	/********** 僶僗儅僗僞 *********/
-	`define BUS_MASTER_CH	   4	 // 僶僗儅僗僞僠儍僱儖悢
-	`define BUS_MASTER_INDEX_W 2	 // 僶僗儅僗僞僀儞僨僢僋僗暆
+	/********** 总线索引定义 *********/
+	`define BUS_MASTER_CH	   4	 // 主控通道数
+	`define BUS_MASTER_INDEX_W 2	 // 主控通道索引宽度
 
-	/********** 僶僗尃偺強桳幰 *********/
-	`define BusOwnerBus		   1:0	 // 僶僗尃忬懺僶僗
-	`define BUS_OWNER_MASTER_0 2'h0	 // 僶僗尃偺強桳幰 丗 僶僗儅僗僞0斣
-	`define BUS_OWNER_MASTER_1 2'h1	 // 僶僗尃偺強桳幰 丗 僶僗儅僗僞1斣
-	`define BUS_OWNER_MASTER_2 2'h2	 // 僶僗尃偺強桳幰 丗 僶僗儅僗僞2斣
-	`define BUS_OWNER_MASTER_3 2'h3	 // 僶僗尃偺強桳幰 丗 僶僗儅僗僞3斣
+	/********** 总线所有者定义 *********/
+	`define BusOwnerBus		   1:0	 // 总线所有者宽度
+	`define BUS_OWNER_MASTER_0 2'h0	 // 总线所有者0
+	`define BUS_OWNER_MASTER_1 2'h1	 // 总线所有者1
+	`define BUS_OWNER_MASTER_2 2'h2	 // 总线所有者2
+	`define BUS_OWNER_MASTER_3 2'h3	 // 总线所有者3
 
-	/********** 僶僗僗儗乕僽 *********/
-	`define BUS_SLAVE_CH	   8	 // 僶僗僗儗乕僽僠儍僱儖悢
-	`define BUS_SLAVE_INDEX_W  3	 // 僶僗僗儗乕僽僀儞僨僢僋僗暆
-	`define BusSlaveIndexBus   2:0	 // 僶僗僗儗乕僽僀儞僨僢僋僗僶僗
-	`define BusSlaveIndexLoc   29:27 // 僶僗僗儗乕僽僀儞僨僢僋僗偺埵抲
+	/********** 总线slave定义 *********/
+	`define BUS_SLAVE_CH	   8	 // slave设备数量
+	`define BUS_SLAVE_INDEX_W  3	 // slave设备数量宽度定义
+	`define BusSlaveIndexBus   2:0	 // slave设备数量宽度定义
+	`define BusSlaveIndexLoc   29:27 // salve设备占据总线地址范围
 
-	`define BUS_SLAVE_0		   0	 // 僶僗僗儗乕僽0斣
-	`define BUS_SLAVE_1		   1	 // 僶僗僗儗乕僽1斣
-	`define BUS_SLAVE_2		   2	 // 僶僗僗儗乕僽2斣
-	`define BUS_SLAVE_3		   3	 // 僶僗僗儗乕僽3斣
-	`define BUS_SLAVE_4		   4	 // 僶僗僗儗乕僽4斣
-	`define BUS_SLAVE_5		   5	 // 僶僗僗儗乕僽5斣
-	`define BUS_SLAVE_6		   6	 // 僶僗僗儗乕僽6斣
-	`define BUS_SLAVE_7		   7	 // 僶僗僗儗乕僽7斣
+	`define BUS_SLAVE_0		   0	 // 
+	`define BUS_SLAVE_1		   1	 // 
+	`define BUS_SLAVE_2		   2	 // 
+	`define BUS_SLAVE_3		   3	 // 
+	`define BUS_SLAVE_4		   4	 // 
+	`define BUS_SLAVE_5		   5	 // 
+	`define BUS_SLAVE_6		   6	 // 
+	`define BUS_SLAVE_7		   7	 // 
 
 `endif
