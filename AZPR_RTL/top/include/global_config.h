a46230388f6c2cd57ecb52800424a8d8aed55aa1
godvmxi <godvmxi@126.com>
2015-06-23 18:29:02 +0800

    fix global config

--- AZPR_RTL/top/include/global_config.h
+++ AZPR_RTL/top/include/global_config.h
@@ -1,63 +1,63 @@
 /*
  -- ============================================================================
  -- FILE NAME	: global_config.h
- -- DESCRIPTION : 全体設定
+ -- DESCRIPTION : global setting para
  -- ----------------------------------------------------------------------------
  -- Revision  Date		  Coding_by	 Comment
- -- 1.0.0	  2011/06/27  suito		 新規作成
+ -- 1.0.0	  2011/06/27  suito		 
  -- ============================================================================
 */
 
 `ifndef __GLOBAL_CONFIG_HEADER__
-	`define __GLOBAL_CONFIG_HEADER__	// インクルードガード
+	`define __GLOBAL_CONFIG_HEADER__	// 
 
 //------------------------------------------------------------------------------
-// 設定項目
+// 
 //------------------------------------------------------------------------------
-	/********** ターゲットデバイス : いずれか1つを選択 **********/
-//	`define TARGET_DEV_MFPGA_SPAR3E		// マルツ評価ボード
-	`define TARGET_DEV_AZPR_EV_BOARD	// AZPRオリジナル評価ボード
+	/********** develop board sellect  **********/
+//	`define TARGET_DEV_MFPGA_SPAR3E		// 
+	`define TARGET_DEV_AZPR_EV_BOARD	//
 
-	/********** リセット極性 : いずれか1つを選択 **********/
+	/********** reset signal active status **********/
 //	`define POSITIVE_RESET				// Active High
 	`define NEGATIVE_RESET				// Active Low
 
-	/********** メモリ制御信号の極性 : いずれか1つを選択 **********/
+	/********** memory active status **********/
 	`define POSITIVE_MEMORY				// Active High
 //	`define NEGATIVE_MEMORY				// Active Low
 
-	/********** I/O設定 : 実装するI/Oを定義**********/
-	`define IMPLEMENT_TIMER				// タイマ
+	/********** I/O device define *********/
+	`define IMPLEMENT_TIMER				// Timer
 	`define IMPLEMENT_UART				// UART
 	`define IMPLEMENT_GPIO				// General Purpose I/O
 
 //------------------------------------------------------------------------------
-// 設定に応じてパラメータを生成
+// signal polarity define
 //------------------------------------------------------------------------------
-	/********** リセット極性 *********/
+	/**********  *********/
 	// Active Low
 	`ifdef POSITIVE_RESET
-		`define RESET_EDGE	  posedge	// リセットエッジ
-		`define RESET_ENABLE  1'b1		// リセット有効
-		`define RESET_DISABLE 1'b0		// リセット無効
+		`define RESET_EDGE	  posedge	// 
+		`define RESET_ENABLE  1'b1		// 
+		`define RESET_DISABLE 1'b0		// 
 	`endif
 	// Active High
 	`ifdef NEGATIVE_RESET
-		`define RESET_EDGE	  negedge	// リセットエッジ
-		`define RESET_ENABLE  1'b0		// リセット有効
-		`define RESET_DISABLE 1'b1		// リセット無効
+		`define RESET_EDGE	  negedge	// 
+		`define RESET_ENABLE  1'b0		// 
+		`define RESET_DISABLE 1'b1		// 
 	`endif
 
 	/********** メモリ制御信号の極性 *********/
 	// Actoive High
 	`ifdef POSITIVE_MEMORY
-		`define MEM_ENABLE	  1'b1		// メモリ有効
-		`define MEM_DISABLE	  1'b0		// メモリ無効
+		`define MEM_ENABLE	  1'b1		// 
+		`define MEM_DISABLE	  1'b0		// 
 	`endif
 	// Active Low
 	`ifdef NEGATIVE_MEMORY
-		`define MEM_ENABLE	  1'b0		// メモリ有効
-		`define MEM_DISABLE	  1'b1		// メモリ無効
+		`define MEM_ENABLE	  1'b0		// 
+		`define MEM_DISABLE	  1'b1		//
 	`endif
 
 `endif
