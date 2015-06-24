ebc7fd95a2fe0fbf3fcc5f9e80d100d03133eca1
bright.jiang <bright.jiang@infotmic.com.cn>
2015-06-24 17:06:26 +0800

    fix bus slave mux

--- AZPR_RTL/bus/rtl/bus_slave_mux.v
+++ AZPR_RTL/bus/rtl/bus_slave_mux.v
@@ -1,90 +1,90 @@
 /*
  -- ============================================================================
  -- FILE NAME	: bus_slave_mux.v
- -- DESCRIPTION : バススレーブマルチプレクサ
+ -- DESCRIPTION : bus slave multiple multiplexer
  -- ----------------------------------------------------------------------------
  -- Revision  Date		  Coding_by	 Comment
- -- 1.0.0	  2011/06/27  suito		 新規作成
+ -- 1.0.0	  2011/06/27  suito
  -- ============================================================================
 */
 
-/********** 共通ヘッダファイル **********/
+/********** common include **********/
 `include "nettype.h"
 `include "stddef.h"
 `include "global_config.h"
 
-/********** 個別ヘッダファイル **********/
+/********** bus define include  **********/
 `include "bus.h"
 
-/********** モジュール **********/
+/********** bus slave mux **********/
 module bus_slave_mux (
-	/********** チップセレクト **********/
-	input  wire				   s0_cs_,	   // バススレーブ0番
-	input  wire				   s1_cs_,	   // バススレーブ1番
-	input  wire				   s2_cs_,	   // バススレーブ2番
-	input  wire				   s3_cs_,	   // バススレーブ3番
-	input  wire				   s4_cs_,	   // バススレーブ4番
-	input  wire				   s5_cs_,	   // バススレーブ5番
-	input  wire				   s6_cs_,	   // バススレーブ6番
-	input  wire				   s7_cs_,	   // バススレーブ7番
-	/********** バススレーブ信号 **********/
-	// バススレーブ0番
-	input  wire [`WordDataBus] s0_rd_data, // 読み出しデータ
-	input  wire				   s0_rdy_,	   // レディ
-	// バススレーブ1番
-	input  wire [`WordDataBus] s1_rd_data, // 読み出しデータ
-	input  wire				   s1_rdy_,	   // レディ
-	// バススレーブ2番
-	input  wire [`WordDataBus] s2_rd_data, // 読み出しデータ
-	input  wire				   s2_rdy_,	   // レディ
-	// バススレーブ3番
-	input  wire [`WordDataBus] s3_rd_data, // 読み出しデータ
-	input  wire				   s3_rdy_,	   // レディ
-	// バススレーブ4番
-	input  wire [`WordDataBus] s4_rd_data, // 読み出しデータ
-	input  wire				   s4_rdy_,	   // レディ
-	// バススレーブ5番
-	input  wire [`WordDataBus] s5_rd_data, // 読み出しデータ
-	input  wire				   s5_rdy_,	   // レディ
-	// バススレーブ6番
-	input  wire [`WordDataBus] s6_rd_data, // 読み出しデータ
-	input  wire				   s6_rdy_,	   // レディ
-	// バススレーブ7番
-	input  wire [`WordDataBus] s7_rd_data, // 読み出しデータ
-	input  wire				   s7_rdy_,	   // レディ
-	/********** バスマスタ共通信号 **********/
-	output reg	[`WordDataBus] m_rd_data,  // 読み出しデータ
-	output reg				   m_rdy_	   // レディ
+	/********** input slave enable signal **********/
+	input  wire				   s0_cs_,	   // slave 0
+	input  wire				   s1_cs_,	   //
+	input  wire				   s2_cs_,	   //
+	input  wire				   s3_cs_,	   //
+	input  wire				   s4_cs_,	   //
+	input  wire				   s5_cs_,	   //
+	input  wire				   s6_cs_,	   //
+	input  wire				   s7_cs_,	   //
+	/********** slave read data and ready signal **********/
+	// slave 0
+	input  wire [`WordDataBus] s0_rd_data, // read data
+	input  wire				   s0_rdy_,	   // read ready
+	// slave 1
+	input  wire [`WordDataBus] s1_rd_data, //
+	input  wire				   s1_rdy_,	   //
+	//
+	input  wire [`WordDataBus] s2_rd_data, //
+	input  wire				   s2_rdy_,	   //
+	//
+	input  wire [`WordDataBus] s3_rd_data, //
+	input  wire				   s3_rdy_,	   //
+	//
+	input  wire [`WordDataBus] s4_rd_data, //
+	input  wire				   s4_rdy_,	   //
+	//
+	input  wire [`WordDataBus] s5_rd_data, //
+	input  wire				   s5_rdy_,	   //
+	//
+	input  wire [`WordDataBus] s6_rd_data, //
+	input  wire				   s6_rdy_,	   //
+	//
+	input  wire [`WordDataBus] s7_rd_data, //
+	input  wire				   s7_rdy_,	   //
+	/********** **********/
+	output reg	[`WordDataBus] m_rd_data,  //
+	output reg				   m_rdy_	   //
 );
 
-	/********** バススレーブマルチプレクサ **********/
+	/********** select the slave channel according enable signal **********/
 	always @(*) begin
-		/* チップセレクトに対応するスレーブの選択 */
-		if (s0_cs_ == `ENABLE_) begin		   // バススレーブ0番
+		/*  */
+		if (s0_cs_ == `ENABLE_) begin		   //
 			m_rd_data = s0_rd_data;
 			m_rdy_	  = s0_rdy_;
-		end else if (s1_cs_ == `ENABLE_) begin // バススレーブ1番
+		end else if (s1_cs_ == `ENABLE_) begin //
 			m_rd_data = s1_rd_data;
 			m_rdy_	  = s1_rdy_;
-		end else if (s2_cs_ == `ENABLE_) begin // バススレーブ2番
+		end else if (s2_cs_ == `ENABLE_) begin //
 			m_rd_data = s2_rd_data;
 			m_rdy_	  = s2_rdy_;
-		end else if (s3_cs_ == `ENABLE_) begin // バススレーブ3番
+		end else if (s3_cs_ == `ENABLE_) begin //
 			m_rd_data = s3_rd_data;
 			m_rdy_	  = s3_rdy_;
-		end else if (s4_cs_ == `ENABLE_) begin // バススレーブ4番
+		end else if (s4_cs_ == `ENABLE_) begin //
 			m_rd_data = s4_rd_data;
 			m_rdy_	  = s4_rdy_;
-		end else if (s5_cs_ == `ENABLE_) begin // バススレーブ5番
+		end else if (s5_cs_ == `ENABLE_) begin //
 			m_rd_data = s5_rd_data;
 			m_rdy_	  = s5_rdy_;
-		end else if (s6_cs_ == `ENABLE_) begin // バススレーブ6番
+		end else if (s6_cs_ == `ENABLE_) begin //
 			m_rd_data = s6_rd_data;
 			m_rdy_	  = s6_rdy_;
-		end else if (s7_cs_ == `ENABLE_) begin // バススレーブ7番
+		end else if (s7_cs_ == `ENABLE_) begin //
 			m_rd_data = s7_rd_data;
 			m_rdy_	  = s7_rdy_;
-		end else begin						   // デフォルト値
+		end else begin						   // default error
 			m_rd_data = `WORD_DATA_W'h0;
 			m_rdy_	  = `DISABLE_;
 		end
