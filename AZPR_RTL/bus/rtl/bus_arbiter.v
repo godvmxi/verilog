865c90a960905bd8e1da99e79adfcdcf35a86ab9
bright.jiang <bright.jiang@infotmic.com.cn>
2015-06-24 17:13:05 +0800

    update bus arbiter

--- AZPR_RTL/bus/rtl/bus_arbiter.v
+++ AZPR_RTL/bus/rtl/bus_arbiter.v
@@ -1,10 +1,10 @@
 /*
  -- ============================================================================
  -- FILE	 : bus_arbiter.v
- -- SYNOPSIS : 总线仲裁器
+ -- SYNOPSIS : bus arbiter
  -- ----------------------------------------------------------------------------
  -- Revision  Date		  Coding_by	 Comment
- -- 1.0.0	  2011/06/27  suito		 
+ -- 1.0.0	  2011/06/27  suito
  -- ============================================================================
 */
 
@@ -16,32 +16,32 @@
 /********** include bus **********/
 `include "bus.h"
 
-/********** 总线仲裁器module **********/
+/********** bus arbiter module  **********/
 module bus_arbiter (
-	/********** 输入信号 **********/
+	/********** clk and reset  **********/
 	input  wire		   clk,		 //
-	input  wire		   reset,	 // 
-	/********** 输出信号 **********/
+	input  wire		   reset,	 //
+	/********** request && ack **********/
 	// for bus master 0
-	input  wire		   m0_req_,	 // 仲裁请求 
-	output reg		   m0_grnt_, // 仲裁响应
+	input  wire		   m0_req_,	 // request
+	output reg		   m0_grnt_, // ack
 	// for bus master 1
-	input  wire		   m1_req_,	 // 
-	output reg		   m1_grnt_, // 
+	input  wire		   m1_req_,	 //
+	output reg		   m1_grnt_, //
 	// for bus master 2
-	input  wire		   m2_req_,	 // 
-	output reg		   m2_grnt_, // 
+	input  wire		   m2_req_,	 //
+	output reg		   m2_grnt_, //
 	// for bus master 3
-	input  wire		   m3_req_,	 // 
+	input  wire		   m3_req_,	 //
 	output reg		   m3_grnt_	 //
 );
 
-	/********** reg 变量声明 **********/
-	reg [`BusOwnerBus] owner;	 // 总线所有者序号
-   
+	/********** reg owner **********/
+	reg [`BusOwnerBus] owner;	 //
+
 	/********** module  **********/
 	always @(*) begin
-		/* 初始化 */
+		/* 锟斤拷始锟斤拷 */
 		m0_grnt_ = `DISABLE_;
 		m1_grnt_ = `DISABLE_;
 		m2_grnt_ = `DISABLE_;
@@ -62,29 +62,29 @@ module bus_arbiter (
 			end
 		endcase
 	end
-   
-	/********** 仲裁所有权检测 **********/
+
+	/**********  **********/
 	always @(posedge clk or `RESET_EDGE reset) begin
 		if (reset == `RESET_ENABLE) begin
 			/* reset */
 			owner <= #1 `BUS_OWNER_MASTER_0;
 		end else begin
-			/* 仲裁所有权检测分配 */
+			/*  */
 			case (owner)
-				`BUS_OWNER_MASTER_0 : begin // 当前所有者为0,优先级: 0>1>2>3
-					/* 按优先级分配所有权 */
+				`BUS_OWNER_MASTER_0 : begin // master 0 mode: 0>1>2>3
+					/* Priority:0>1>2>3 */
 					if (m0_req_ == `ENABLE_) begin			//
 						owner <= #1 `BUS_OWNER_MASTER_0;
-					end else if (m1_req_ == `ENABLE_) begin // 
+					end else if (m1_req_ == `ENABLE_) begin //
 						owner <= #1 `BUS_OWNER_MASTER_1;
-					end else if (m2_req_ == `ENABLE_) begin // 
+					end else if (m2_req_ == `ENABLE_) begin //
 						owner <= #1 `BUS_OWNER_MASTER_2;
-					end else if (m3_req_ == `ENABLE_) begin // 
+					end else if (m3_req_ == `ENABLE_) begin //
 						owner <= #1 `BUS_OWNER_MASTER_3;
 					end
 				end
-				`BUS_OWNER_MASTER_1 : begin // 当前所有者为1,
-					/*优先级: 1>2>3>0 */
+				`BUS_OWNER_MASTER_1 : begin // mater mode 1
+					/* Priority:1>2>3>0 */
 					if (m1_req_ == `ENABLE_) begin			//
 						owner <= #1 `BUS_OWNER_MASTER_1;
 					end else if (m2_req_ == `ENABLE_) begin //
@@ -95,27 +95,27 @@ module bus_arbiter (
 						owner <= #1 `BUS_OWNER_MASTER_0;
 					end
 				end
-				`BUS_OWNER_MASTER_2 : begin // 当前所有者为2
-					/* 优先级: 2>3>0>1 */
-					if (m2_req_ == `ENABLE_) begin			// 
+				`BUS_OWNER_MASTER_2 : begin // mode 2
+					/* Priority: 2>3>0>1 */
+					if (m2_req_ == `ENABLE_) begin			//
 						owner <= #1 `BUS_OWNER_MASTER_2;
-					end else if (m3_req_ == `ENABLE_) begin // 
+					end else if (m3_req_ == `ENABLE_) begin //
 						owner <= #1 `BUS_OWNER_MASTER_3;
-					end else if (m0_req_ == `ENABLE_) begin // 
+					end else if (m0_req_ == `ENABLE_) begin //
 						owner <= #1 `BUS_OWNER_MASTER_0;
 					end else if (m1_req_ == `ENABLE_) begin //
 						owner <= #1 `BUS_OWNER_MASTER_1;
 					end
 				end
-				`BUS_OWNER_MASTER_3 : begin // 当前所有者为3
-					/* 优先级: 3>0>1>2 */
-					if (m3_req_ == `ENABLE_) begin			// 
+				`BUS_OWNER_MASTER_3 : begin // mode 3
+					/* Priority: 3>0>1>2 */
+					if (m3_req_ == `ENABLE_) begin			//
 						owner <= #1 `BUS_OWNER_MASTER_3;
-					end else if (m0_req_ == `ENABLE_) begin // 
+					end else if (m0_req_ == `ENABLE_) begin //
 						owner <= #1 `BUS_OWNER_MASTER_0;
-					end else if (m1_req_ == `ENABLE_) begin // 
+					end else if (m1_req_ == `ENABLE_) begin //
 						owner <= #1 `BUS_OWNER_MASTER_1;
-					end else if (m2_req_ == `ENABLE_) begin // 
+					end else if (m2_req_ == `ENABLE_) begin //
 						owner <= #1 `BUS_OWNER_MASTER_2;
 					end
 				end
