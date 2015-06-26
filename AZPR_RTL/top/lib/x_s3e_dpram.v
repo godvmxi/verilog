6da1605b1430fd06b4b53f1fbd1641d2656e274f
bright.jiang <bright.jiang@infotmic.com.cn>
2015-06-26 15:47:22 +0800

    update s3e dpram

--- AZPR_RTL/top/lib/x_s3e_dpram.v
+++ AZPR_RTL/top/lib/x_s3e_dpram.v
@@ -1,63 +1,63 @@
 /*
  -- ============================================================================
  -- FILE NAME	: x_s3e_dpram.v
- -- DESCRIPTION : Xilinx Spartan-3E Dual Port RAM 疑似モデル
+ -- DESCRIPTION : Xilinx Spartan-3E Dual Port RAM
  -- ----------------------------------------------------------------------------
  -- Revision  Date		  Coding_by	 Comment
- -- 1.0.0	  2011/06/27  suito		 新規作成
+ -- 1.0.0	  2011/06/27  suito
  -- ============================================================================
 */
 
-/********** 共通ヘッダファイル **********/
+/********** common include  **********/
 `include "nettype.h"
 `include "stddef.h"
 `include "global_config.h"
 
-/********** 個別ヘッダファイル **********/
+/********** spm define include  **********/
 `include "spm.h"
 
-/********** モジュール **********/
+/********** module define  **********/
 module x_s3e_dpram (
-	/********** ポート A **********/
-	input  wire				   clka,  // クロック
-	input  wire [`SpmAddrBus]  addra, // アドレス
-	input  wire [`WordDataBus] dina,  // 書き込みデータ
-	input  wire				   wea,	  // 書き込み有効
-	output reg	[`WordDataBus] douta, // 読み出しデータ
-	/********** ポート B **********/
-	input  wire				   clkb,  // クロック
-	input  wire [`SpmAddrBus]  addrb, // アドレス
-	input  wire [`WordDataBus] dinb,  // 書き込みデータ
-	input  wire				   web,	  // 書き込み有効
-	output reg	[`WordDataBus] doutb  // 読み出しデータ
+	/********** port A **********/
+	input  wire				   clka,  // clk
+	input  wire [`SpmAddrBus]  addra, // address
+	input  wire [`WordDataBus] dina,  // data in
+	input  wire				   wea,	  // write read  :write
+	output reg	[`WordDataBus] douta, // data out
+	/********** port B **********/
+	input  wire				   clkb,  //
+	input  wire [`SpmAddrBus]  addrb, //
+	input  wire [`WordDataBus] dinb,  //
+	input  wire				   web,	  //
+	output reg	[`WordDataBus] doutb  //
 );
 
-	/********** メモリ **********/
+	/********** mem area and size **********/
 	reg [`WordDataBus] mem [0:`SPM_DEPTH-1];
 
-	/********** メモリアクセス（ポート A） **********/
+	/**********  **********/
 	always @(posedge clka) begin
-		// 読み出しアクセス
+		// write
 		if ((web == `ENABLE) && (addra == addrb)) begin
-			douta	  <= #1 dinb;
+			douta	  <= #1 dinb; //direct douta  from b
 		end else begin
 			douta	  <= #1 mem[addra];
 		end
-		// 書き込みアクセス
+		// for read
 		if (wea == `ENABLE) begin
 			mem[addra]<= #1 dina;
 		end
 	end
 
-	/********** メモリアクセス（ポート B） **********/
+	/**********  **********/
 	always @(posedge clkb) begin
-		// 読み出しアクセス
+		//
 		if ((wea == `ENABLE) && (addrb == addra)) begin
 			doutb	  <= #1 dina;
 		end else begin
 			doutb	  <= #1 mem[addrb];
 		end
-		// 書き込みアクセス
+		// 
 		if (web == `ENABLE) begin
 			mem[addrb]<= #1 dinb;
 		end
