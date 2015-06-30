c4a16a7fffb04f35f77bba6aa394e6f26aa3c20e
bright.jiang <bright.jiang@infotmic.com.cn>
2015-06-30 16:01:01 +0800

    update cpu if reg

--- AZPR_RTL/cpu/rtl/if_reg.v
+++ AZPR_RTL/cpu/rtl/if_reg.v
@@ -1,60 +1,60 @@
 /*
  -- ============================================================================
  -- FILE NAME	: if_reg.v
- -- DESCRIPTION : IFステージパイプラインレジスタ
+ -- DESCRIPTION : IF stage reg
  -- ----------------------------------------------------------------------------
  -- Revision  Date		  Coding_by	 Comment
- -- 1.0.0	  2011/06/27  suito		 新規作成
+ -- 1.0.0	  2011/06/27  suito
  -- ============================================================================
 */
 
-/********** 共通ヘッダファイル **********/
+/********** common include  **********/
 `include "nettype.h"
 `include "global_config.h"
 `include "stddef.h"
 
-/********** 個別ヘッダファイル **********/
+/********** isa cpu include  **********/
 `include "isa.h"
 `include "cpu.h"
 
-/********** モジュール **********/
+/********** if reg module define **********/
 module if_reg (
-	/********** クロック & リセット **********/
-	input  wire				   clk,		   // クロック
-	input  wire				   reset,	   // 非同期リセット
-	/********** フェッチデータ **********/
-	input  wire [`WordDataBus] insn,	   // フェッチした命令
-	/********** パイプライン制御信号 **********/
-	input  wire				   stall,	   // ストール
-	input  wire				   flush,	   // フラッシュ
-	input  wire [`WordAddrBus] new_pc,	   // 新しいプログラムカウンタ
-	input  wire				   br_taken,   // 分岐の成立
-	input  wire [`WordAddrBus] br_addr,	   // 分岐先アドレス
-	/********** IF/IDパイプラインレジスタ **********/
-	output reg	[`WordAddrBus] if_pc,	   // プログラムカウンタ
-	output reg	[`WordDataBus] if_insn,	   // 命令
-	output reg				   if_en	   // パイプラインデータの有効
+	/********** clk && reset  **********/
+	input  wire				   clk,		   //
+	input  wire				   reset,	   //
+	/**********  **********/
+	input  wire [`WordDataBus] insn,	   // instruction
+	/********** control sigal **********/
+	input  wire				   stall,	   // delay
+	input  wire				   flush,	   // flush
+	input  wire [`WordAddrBus] new_pc,	   //
+	input  wire				   br_taken,   // Branch establishment
+	input  wire [`WordAddrBus] br_addr,	   // Branch address
+	/********** IF/ID register  **********/
+	output reg	[`WordAddrBus] if_pc,	   //
+	output reg	[`WordDataBus] if_insn,	   //
+	output reg				   if_en	   // Pipeline Data significance
 );
 
-	/********** パイプラインレジスタ **********/
+	/**********  **********/
 	always @(posedge clk or `RESET_EDGE reset) begin
-		if (reset == `RESET_ENABLE) begin 
-			/* 非同期リセット */
+		if (reset == `RESET_ENABLE) begin
+			/*  */
 			if_pc	<= #1 `RESET_VECTOR;
 			if_insn <= #1 `ISA_NOP;
 			if_en	<= #1 `DISABLE;
 		end else begin
-			/* パイプラインレジスタの更新 */
-			if (stall == `DISABLE) begin 
-				if (flush == `ENABLE) begin				// フラッシュ
+			/*  */
+			if (stall == `DISABLE) begin
+				if (flush == `ENABLE) begin				// 
 					if_pc	<= #1 new_pc;
 					if_insn <= #1 `ISA_NOP;
 					if_en	<= #1 `DISABLE;
-				end else if (br_taken == `ENABLE) begin // 分岐の成立
+				end else if (br_taken == `ENABLE) begin //
 					if_pc	<= #1 br_addr;
 					if_insn <= #1 insn;
 					if_en	<= #1 `ENABLE;
-				end else begin							// 次のアドレス
+				end else begin							// get next pc value
 					if_pc	<= #1 if_pc + 1'd1;
 					if_insn <= #1 insn;
 					if_en	<= #1 `ENABLE;
