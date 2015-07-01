56d44bda7487cb5295c3ce3d6c9b01118900c025
bright.jiang <bright.jiang@infotmic.com.cn>
2015-07-01 18:46:07 +0800

    save

--- AZPR_RTL/top/lib/x_s3e_dcm.v
+++ AZPR_RTL/top/lib/x_s3e_dcm.v
@@ -1,28 +1,28 @@
 /*
  -- ============================================================================
  -- FILE NAME	: x_s3e_dcm.v
- -- DESCRIPTION : Xilinx Spartan-3E DCM 疑似モデル
+ -- DESCRIPTION : Xilinx Spartan-3E DCM
  -- ----------------------------------------------------------------------------
  -- Revision  Date		  Coding_by	 Comment
- -- 1.0.0	  2011/06/27  suito		 新規作成
+ -- 1.0.0	  2011/06/27  suito
  -- ============================================================================
 */
 
-/********** 共通ヘッダファイル **********/
+/**********  **********/
 `include "nettype.h"
 
-/********** モジュール **********/
+/**********  **********/
 module x_s3e_dcm (
-	input  wire CLKIN_IN,		 // 既定クロック
-	input  wire RST_IN,			 // リセット
-	output wire CLK0_OUT,		 // クロック（φ0）
-	output wire CLK180_OUT,		 // クロック（φ180）
-	output wire LOCKED_OUT		 // ロック
+	input  wire CLKIN_IN,		 //
+	input  wire RST_IN,			 //
+	output wire CLK0_OUT,		 //
+	output wire CLK180_OUT,		 //
+	output wire LOCKED_OUT		 //
 );
 
-	/********** クロック出力 **********/
+	/**********  **********/
 	assign CLK0_OUT	  = CLKIN_IN;
 	assign CLK180_OUT = ~CLKIN_IN;
 	assign LOCKED_OUT = ~RST_IN;
-   
+
 endmodule
--- AZPR_RTL/top/rtl/clk_gen.v
+++ AZPR_RTL/top/rtl/clk_gen.v
@@ -1,48 +1,48 @@
-/* 
+/*
  -- ============================================================================
  -- FILE NAME	: clk_gen.v
- -- DESCRIPTION : クロック生成モジュール
+ -- DESCRIPTION :
  -- ----------------------------------------------------------------------------
  -- Revision  Date		  Coding_by	 Comment
- -- 1.0.0	  2011/06/27  suito		 新規作成
+ -- 1.0.0	  2011/06/27  suito
  -- ============================================================================
 */
 
-/********** 共通ヘッダファイル **********/
+/********** common include  **********/
 `include "nettype.h"
 `include "stddef.h"
 `include "global_config.h"
 
-/********** モジュール **********/
+/********** clock gen module  **********/
 module clk_gen (
-	/********** クロック & リセット **********/
-	input wire	clk_ref,   // 基底クロック
-	input wire	reset_sw,  // リセットスイッチ
-	/********** 生成クロック **********/
-	output wire clk,	   // クロック
-	output wire clk_,	   // 反転クロック
-	/********** チップリセット **********/
-	output wire chip_reset // チップリセット
+	/********** clk && reset  **********/
+	input wire	clk_ref,   // main clk
+	input wire	reset_sw,  // async reset
+	/********** output  **********/
+	output wire clk,	   // out clk
+	output wire clk_,	   // out nege clk
+	/********** output reset  **********/
+	output wire chip_reset // clk reset output
 );
 
-	/********** 内部信号 **********/
-	wire		locked;	   // ロック
-	wire		dcm_reset; // リセット
+	/**********  **********/
+	wire		locked;	   // clk locked
+	wire		dcm_reset; // 
 
-	/********** リセットの生成 **********/
-	// DCMリセット
+	/**********  **********/
+	//
 	assign dcm_reset  = (reset_sw == `RESET_ENABLE) ? `ENABLE : `DISABLE;
-	// チップリセット
+	//
 	assign chip_reset = ((reset_sw == `RESET_ENABLE) || (locked == `DISABLE)) ?
 							`RESET_ENABLE : `RESET_DISABLE;
 
 	/********** Xilinx DCM (Digitl Clock Manager) **********/
 	x_s3e_dcm x_s3e_dcm (
-		.CLKIN_IN		 (clk_ref),	  // 基底クロック
-		.RST_IN			 (dcm_reset), // DCMリセット
-		.CLK0_OUT		 (clk),		  // クロック
-		.CLK180_OUT		 (clk_),	  // 反転クロック
-		.LOCKED_OUT		 (locked)	  // ロック
+		.CLKIN_IN		 (clk_ref),	  //
+		.RST_IN			 (dcm_reset), //
+		.CLK0_OUT		 (clk),		  //
+		.CLK180_OUT		 (clk_),	  //
+		.LOCKED_OUT		 (locked)	  //
    );
 
 endmodule
